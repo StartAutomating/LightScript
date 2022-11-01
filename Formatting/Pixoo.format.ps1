fb659cf49adeeab576209bebcee80286b1c0ccff
James Brundage <@github.com>
2022-10-31 18:00:38 -0700

    Connect-Pixoo:  No longer storing state (Fixes #56)

--- Formatting/Pixoo.format.ps1
+++ Formatting/Pixoo.format.ps1
@@ -1,3 +1,1 @@
-Write-formatView -TypeName Pixoo -Property IPAddress, MACAddress, Brightness
-
-Write-formatView -TypeName Pixoo -Property RotationFlag, GyrateAngle, MirrorFlag -Name Orientation
+Write-formatView -TypeName Pixoo -Property IPAddress, MACAddress
