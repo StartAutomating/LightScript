f81eae3b77d909fd77df84b5848adaf1e87a57b4
James Brundage <@github.com>
2022-11-10 13:15:46 -0800

    Adding Connect-LaMetricTime (Fixes #67)

---
+++ Functions/LaMetric/Connect-LaMetricTime.ps.ps1
@@ -0,0 +1,86 @@
+﻿function Connect-LaMetricTime
+{
+    <#
+    .Synopsis
+        Connects to a LaMetric clock
+    .Description
+        Connects to a LaMetric clock.
+
+        LaMetric Time devices require a local ApiKey.
+        
+        Unfortunately, there is no way to get this key automatically.
+        
+        To Connect-LaMetricTime, you'll need to visit [developer.lametric.com](https://developer.lametric.com) and sign in.
+
+        You will find API Keys for your devices at [developer.lametric.com/user/devices](https://developer.lametric.com/user/devices).
+    .Example
+        Connect-LaMetric -IPAddress $laMetricIP -ApiKey $myApiKey -PassThru
+    .Link
+        Get-LaMetricTime
+    #>
+    [OutputType([Nullable], [PSObject])]
+    param(
+    # The IP Address for the Twinkly device.  This can be discovered thru the phone user interface.
+    [Parameter(Mandatory,Position=0,ValueFromPipelineByPropertyName)]
+    [Alias('LaMetricTimeIPAddress')]
+    [IPAddress]
+    $IPAddress,
+
+    # The IP Address for the Twinkly device.  This can be discovered thru the phone user interface.
+    [Parameter(Mandatory,Position=1,ValueFromPipelineByPropertyName)]
+    [Alias('LaMetricTimeApiKey')]
+    [string]
+    $ApiKey,
+
+    # If set, will output the connection information.
+    [switch]
+    $PassThru
+    )
+
+    begin {
+        if ($home) {
+            $lightScriptRoot = Join-Path $home -ChildPath LightScript
+        }
+    }
+
+    process {
+        $laMetricB64Key = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes("dev:$ApiKey")) 
+        $ipAndPort      = "${ipAddress}:8080"
+
+        #region Connect to the Device
+        $laMetricDevice = 
+            http://$ipAndPort/api/v2/device -Headers @{
+                Authorization = "Basic $laMetricB64Key"            
+            }
+
+        # If we could not connect, return
+        if (-not $laMetricDevice) {
+            return
+        }
+        #endregion Connect to the Device
+        
+        if ($laMetricDevice) {            
+            #region Save Device Information
+            [decorate(PSTypeName='LaMetric.Time',Clear)]$laMetricDevice
+
+            if ($home -and $laMetricDevice) {
+                if (-not (Test-Path $lightScriptRoot)) {
+                    $createLightScriptDir = New-Item -ItemType Directory -Path $lightScriptRoot
+                    if (-not $createLightScriptDir) { return }
+                }
+
+
+                $laMetricDataFile = Join-Path $lightScriptRoot ".$($laMetricDevice.serial_number).LaMetricTime.clixml"
+
+                $laMetricDevice |
+                    Add-Member NoteProperty IpAddress $IPAddress -Force -PassThru |
+                    Add-Member NoteProperty ApiKey $laMetricB64Key -Force -PassThru |
+                    Export-Clixml -Path $laMetricDataFile
+            }
+            #endregion Save Device Information
+            if ($PassThru) {
+                $laMetricDevice
+            }
+        }
+    }
+}
