02b285d52d8e5e21b5d92586e00a9a3cdd72cb01
James Brundage <@github.com>
2022-11-10 19:07:11 -0800

    Adding Get-LaMetricTime -Notification (Fixes #73)

--- Functions/LaMetric/Get-LaMetricTime.ps.ps1
+++ Functions/LaMetric/Get-LaMetricTime.ps.ps1
@@ -31,6 +31,12 @@ function Get-LaMetricTime
     [switch]
     $Display,
 
+    # If set, will get LaMetric Time notifications
+    [Parameter(Mandatory,ParameterSetName='api/v2/device/notifications')]
+    [Alias('Notifications')]
+    [switch]
+    $Notification,
+
     # If set, will get details about a particular package of an LaMetric Time device.
     [Parameter(Mandatory,ParameterSetName='api/v2/device/apps/$Package',ValueFromPipelineByPropertyName)]    
     [string]
@@ -80,7 +86,10 @@ function Get-LaMetricTime
                 $typename  = 
                     if ($friendlyParameterSetNames[$PSCmdlet.ParameterSetName]) {
                         $friendlyParameterSetNames[$PSCmdlet.ParameterSetName]
-                    } else { @($endpoint -split '/')[-1]}
+                    } else { 
+                        $lastSegment = @($endpoint -split '/')[-1]
+                        ($lastSegment.Substring(0,1).ToUpper() + $lastSegment.Substring(1)) -replace 's$'
+                    }
                 #region Connect to the Device
                 
                 http://$ipAndPort/api/$endpoint -Headers @{
