6eabd0eadfc95e2246e5af13f5099a312311d6b3
James Brundage <@github.com>
2022-11-11 20:49:22 -0800

    Set-LaMetricTime: -Notification support (Fixes #85)

--- Functions/LaMetric/Set-LaMetricTime.ps.ps1
+++ Functions/LaMetric/Set-LaMetricTime.ps.ps1
@@ -43,6 +43,50 @@ function Set-LaMetricTime
     [Alias('NextApp','Next')]
     [switch]
     $NextApplication,
+
+    # One or more messages of notification text
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string[]]
+    $NotificationText,
+
+    # One or more notification icons.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [Alias('IconID')]
+    [string[]]
+    $NotificationIcon,
+
+    # The duration of the notification.
+    # By default, 15 seconds.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [timespan]
+    $NotificationDuration = "00:00:15",
+
+    # The number of times to display the notification.
+    # Zero or less will be considered an indefinite notification
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [Alias('LoopCount')]
+    [int]
+    $NotificationLoopCount,
+
+    # If set, will indefinitely loop the notification.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [Alias('Loop')]
+    [switch]
+    $LoopNotification,
+
+    # If provided, will play a sound with the notification.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [ValidateSet(
+        'alarm1','alarm2','alarm3','alarm4','alarm5','alarm6','alarm7',
+        'alarm8','alarm9','alarm10','alarm11','alarm12','alarm13',
+        'bicycle','car','cash','cat','dog','dog2','energy','knock-knock','letter_email',
+        'lose1','lose2','negative1','negative2','negative3','negative4','negative5',
+        'notification','notification2','notification3','notification4',
+        'open_door','positive1','positive2','positive3','positive4','positive5','positive6',
+        'statistic','thunder','water1','water2','win','win2','wind','wind_short'
+    )]
+    [string]
+    $NotificationSound,
     
     # Sets a Timer on the LaMetric device, using the built-in Countdown app.
     [Parameter(ValueFromPipelineByPropertyName)]
@@ -70,21 +114,20 @@ function Set-LaMetricTime
 
         foreach ($ip in $IPAddress) {
             $laMetricB64Key = $script:LaMetricTimeCache["$ip"].ApiKey
+            $ipAndPort = "${ip}:8080"
+            $headers = @{Authorization = "Basic $laMetricB64Key"}
             #region Timer
-            if ($timer.TotalSeconds -ge 1) {
-                $ipAndPort = "${ip}:8080"
+            if ($timer.TotalSeconds -ge 1) {                
                 $endpoint  = "api/v2/device/apps"
                 $appAndWiget = "com.lametric.countdown/widgets/f03ea1ae1ae5f85b390b460f55ba8061/actions"
-                post http://$ipAndPort/$endpoint/$appAndWiget -Headers @{
-                    Authorization = "Basic $laMetricB64Key"
-                } -Body ([Ordered]@{
+                post http://$ipAndPort/$endpoint/$appAndWiget -Body ([Ordered]@{
                     id = "countdown.configure"
                     params = [Ordered]@{
                         duration  = [int]$timer.TotalSeconds
                         start_now = $true                    
                     }
                     activate = $true
-                } | ConvertTo-Json)
+                } | ConvertTo-Json) -Headers $headers
             }
             #endregion Timer
 
@@ -92,61 +135,126 @@ function Set-LaMetricTime
             if ($Stopwatch) {
                 if ($Stopwatch -eq 'Stop') {
                     $Stopwatch = "Pause"
-                }
-                $ipAndPort = "${ip}:8080"
+                }                
                 $endpoint  = "api/v2/device/apps"
                 $appAndWiget = "com.lametric.stopwatch/widgets/b1166a6059640bf76b9dfe0455ba8062/actions"
-                post http://$ipAndPort/$endpoint/$appAndWiget -Headers @{
-                    Authorization = "Basic $laMetricB64Key"
-                } -Body ([Ordered]@{
+                post http://$ipAndPort/$endpoint/$appAndWiget -Body ([Ordered]@{
                     id = "stopwatch.$($Stopwatch.ToLower())"
                     params = [Ordered]@{}
                     activate = $true
-                } | ConvertTo-Json)
+                } | ConvertTo-Json) -Headers $headers
             }
             #endregion Stopwatch
 
             #region Weather
-            if ($Weather) {
-                $ipAndPort = "${ip}:8080"
+            if ($Weather) {                
                 $endpoint  = "api/v2/device/apps"
                 $appAndWiget = "com.lametric.weather/widgets/380375c4b12c16e3adafb48355ba8061/activate"
-                put http://$ipAndPort/$endpoint/$appAndWiget -Headers @{
-                    Authorization = "Basic $laMetricB64Key"
-                }
+                put http://$ipAndPort/$endpoint/$appAndWiget -Headers $headers
             }
             #endregion Weather
 
             #region -Clock
-            if ($Clock) {
-                $ipAndPort = "${ip}:8080"
+            if ($Clock) {                
                 $endpoint  = "api/v2/device/apps"
                 $appAndWiget = "com.lametric.clock/widgets/08b8eac21074f8f7e5a29f2855ba8060/activate"
-                put http://$ipAndPort/$endpoint/$appAndWiget -Headers @{
-                    Authorization = "Basic $laMetricB64Key"
-                }
+                put http://$ipAndPort/$endpoint/$appAndWiget -Headers $headers
             }
             #endregion Clock
 
             #region -NextApplication
-            if ($NextApplication) {
-                $ipAndPort = "${ip}:8080"
+            if ($NextApplication) {                
                 $endpoint  = "api/v2/device/apps/next"
-                put http://$ipAndPort/$endpoint -Headers @{
-                    Authorization = "Basic $laMetricB64Key"
-                }
+                put http://$ipAndPort/$endpoint -Headers $headers
             }
             #endregion NextApplication
 
             #region LastApplication
             if ($LastApplication) {
-                $ipAndPort = "${ip}:8080"
                 $endpoint  = "api/v2/device/apps/prev"
-                put http://$ipAndPort/$endpoint -Headers @{
-                    Authorization = "Basic $laMetricB64Key"
-                }
+                put http://$ipAndPort/$endpoint -Headers $headers
             }
             #endregion LastApplication
+
+            #region Notifications
+            if ($NotificationText -or $NotificationIcon) {
+                $maxFrameCount = [Math]::max($NotificationText.Length, $NotificationIcon.Length)
+                $notificationFrames = 
+                    @(for ($frameNumber =0 ; $frameNumber -lt $maxFrameCount; $frameNumber++) {
+                        $frameInfo = [Ordered]@{}
+                        if ($NotificationText -and $NotificationText[$frameNumber]) {
+                            if ($NotificationText[$frameNumber] -match '^(?<p>\d+)%$') {
+                                $frameInfo.goalData = [Ordered]@{
+                                    start = 0
+                                    current = [int]$matches.p
+                                    end = 100
+                                    unit = "%"
+                                }
+                            } else {
+                                $frameInfo.text = $NotificationText[$frameNumber]
+                            }                            
+                        }
+                        if ($NotificationIcon -and $NotificationIcon[$frameNumber]) {
+                            $icon = $NotificationIcon[$frameNumber]
+                            if ($icon -like 'http*') {
+
+                            }
+                            elseif ((($icon -replace '\D') -as [int]) -ge 1) {
+                                $frameInfo.icon = $icon
+                            }
+                            elseif ($icon.Contains([IO.Path]::DirectorySeparatorChar))
+                            {
+                                $iconItem = Get-Item -Path $icon
+                                if (-not $iconItem) {
+                                    continue
+                                }
+                                if ($iconItem.Extension -notin '.gif', '.png') {
+                                    Write-Error "Icon '$icon' must be a .gif or .png"
+                                    continue
+                                }                                            
+                                $frameInfo.icon = "image/$($iconItem.Extension.TrimStart('.'));base64,$(
+                                    [Convert]::ToBase64String([IO.File]::ReadAllBytes($iconItem.FullName))
+                                )"
+                            }                                        
+                        }
+                        $frameInfo 
+                    })
+                
+                $notificationBody = [Ordered]@{                    
+                    priority = 'info'
+                    icon_type = 'none'
+                    lifetime  = 
+                        if ($notificationDuration) { [int]$notificationDuration.TotalMilliseconds} else { 10000 }
+                    model = 
+                        [Ordered]@{            
+                            frames = $notificationFrames
+                        }
+                }
+
+                if ($LoopNotification) {
+                    $notificationBody.model.cycles = 0
+                }
+                if ($NotificationLoopCount) {
+                    $notificationBody.model.cycles = [int][math]::min(0, $NotificationLoopCount)
+                }
+
+                if ($NotificationSound) {
+                    $notificationBody.model.sound = [Ordered]@{}
+                    $notificationBody.model.sound.category = 
+                        if ($NotificationSound -like 'alarm*') {
+                            'alarms'
+                        } else {
+                            'notifications'
+                        }
+                    $notificationBody.model.sound.id = $NotificationSound                    
+                }
+
+                post http://$ipAndPort/api/v2/device/notifications -body (
+                    $notificationBody | ConvertTo-Json -Depth 10
+                ) -Headers $headers
+            }
+            #endregion Notifications
         }
     }
 }
+
