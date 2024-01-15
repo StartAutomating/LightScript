ce765d1370776b6c5e9483594e72cbe1ff38ea9a
James Brundage <@github.com>
2024-01-14 16:25:23 -0800

    feat: Get-Awtrix ( Fixes #102 )

---
+++ Functions/Awtrix/Get-Awtrix.ps1
@@ -0,0 +1,67 @@
+function Get-Awtrix
+{
+    <#
+    .Synopsis
+        Gets Awtrix Devices
+    .Description
+        Gets saved Awtrix Devices
+    .Example
+        Get-Awtrix
+    .LINK
+        Connect-Awtrix
+    .LINK
+        Set-Awtrix
+    #>
+    [CmdletBinding(DefaultParameterSetName="ListDevices")]
+    param(
+    # The IP Address for the Awtrix device.
+    # This can be discovered thru the phone user interface or by using Find-Awtrix.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [Alias('AwtrixIPAddress')]
+    [IPAddress[]]
+    $IPAddress,
+    
+    # If set, will clear any cached results.
+    [switch]
+    $Force
+    )
+
+    begin {
+        if (-not $script:AwtrixCache) {
+            $script:AwtrixCache = @{}
+        }
+
+        if (-not $script:AwtrixDataCache -or $force) {
+            $script:AwtrixDataCache = @{}
+        }
+        if ($home) {
+            $lightScriptRoot = Join-Path $home -ChildPath LightScript
+        }
+    }
+    
+    process {
+        #region Default to All Devices
+        if (-not $IPAddress) { # If no -IPAddress was passed
+            if ($home) {
+                # Read all .Awtrix.clixml files beneath your LightScript directory.
+                Get-ChildItem -Path $lightScriptRoot -ErrorAction SilentlyContinue -Filter *.awtrix.clixml -Force |
+                    Import-Clixml |
+                    ForEach-Object {
+                        if (-not $_) { return }
+                        $AwtrixConnection = $_                        
+                        $script:AwtrixCache["$($AwtrixConnection.IPAddress)"] = $AwtrixConnection
+                    }
+
+                $IPAddress = $script:AwtrixCache.Keys # The keys of the device cache become the -IPAddress.
+            }
+            if (-not $IPAddress) { # If we still have no -IPAddress                
+                return #  return.
+            }
+        }
+        #endregion Default to All Devices
+
+        if ($PSCmdlet.ParameterSetName -eq 'ListDevices') {
+            return $script:AwtrixCache.Values
+        }
+    }
+}
