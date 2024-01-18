87e24ecd167b21225c32f41cd686c88a8ec5ed08
James Brundage <@github.com>
2024-01-18 11:05:53 -0800

    chore: Moving LightScript.GitHubWorkflow into /Build ( Fixes #108 )

--- Build/LightScript.GitHubWorkflow.psdevops.ps1
+++ Build/LightScript.GitHubWorkflow.psdevops.ps1
@@ -1,6 +1,9 @@
 ﻿#requires -Module PSDevOps
 Push-Location $PSScriptRoot
-Import-BuildStep -Module LightScript
+Import-BuildStep -SourcePath (
+    Join-Path $PSScriptRoot 'GitHub'
+) -BuildSystem GitHubWorkflow
+
 New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish, BuildModule |
     Set-Content .\.github\workflows\TestAndPublish.yml -Encoding UTF8 -PassThru
 
